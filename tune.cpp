889a87e53d7808491a24dde06c6fb02d4e2050b4
kotonohaparty <hellcat@foxmail.com>
2013-05-26 07:51:22 +0800

    Create tune.cpp

---
+++ tune.cpp
@@ -0,0 +1,24 @@
+//#include "tune.h"
+/*
+tune::tune()
+{}
+
+int tune::add_bar(bar &br, int n)
+{}
+
+int tune::delete_bar(int n)
+{}
+
+bool tune::useful()
+{
+  return use;
+}
+
+void tune::clean()
+{
+	use = false;
+}
+
+void tune::show()
+{}
+*/
