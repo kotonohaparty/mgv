068469bda0b8a58838d8f1cbb770f99d07b83865
kotonohaparty <hellcat@foxmail.com>
2013-05-26 07:46:27 +0800

    Create bar.h

---
+++ bar.h
@@ -0,0 +1,30 @@
+#ifndef _BAR_H_
+#define _BAR_H_
+
+#include "beat.h"
+
+/* for tune */
+#define BEAT_PER_BAR 8
+
+/*
+class bar, every tune includes eight beat. [6][22]
+*/
+class bar
+{
+public:
+  bar();
+	int add_beat(const beat &bt, const uchar n);
+	int delete_beat(const uchar n);
+	void clean();
+	bool useful();
+
+	void show();
+
+private:
+	beat_vector beat_vtr;
+	bool use;
+};
+
+typedef std::vector<bar*> bar_vector;
+
+#endif
